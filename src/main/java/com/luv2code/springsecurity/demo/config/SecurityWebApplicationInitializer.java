a1387f56370b68e8d520b36ef90f8bab89549821
e1500948 <tynshjt@gmail.com>
2018-11-14 01:02:46 +0200

    mvc and security initial config

---
+++ src/main/java/com/luv2code/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,9 @@
+package com.luv2code.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+//just extends to kinda trigger
+public class SecurityWebApplicationInitializer 
+					extends AbstractSecurityWebApplicationInitializer {
+
+}
