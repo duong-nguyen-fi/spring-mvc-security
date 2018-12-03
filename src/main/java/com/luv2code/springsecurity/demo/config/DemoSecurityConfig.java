34e59c2cf5779edddd13472a27415df99111910b
e1500948 <tynshjt@gmail.com>
2018-12-03 03:47:19 +0200

    add authentication with jdbc support - plain text

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.luv2code.springsecurity.demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,15 +16,13 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource securityDataSource;
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// TODO Auto-generated method stub
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		auth.inMemoryAuthentication()
-		.withUser(users.username("john").password("test123").roles("USER"))
-		.withUser(users.username("mary").password("test123").roles("USER","MANAGER"))
-		.withUser(users.username("susan").password("test123").roles("ADMIN", "USER"));
+		auth.jdbcAuthentication().dataSource(securityDataSource);
 	}
 
 	@Override
